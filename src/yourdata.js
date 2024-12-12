45cdd13aa43d2d33755309a2318d6b06e4ba7f73
Cristopher Cervantes <cristopher.cervantes@pslgroup.com>
2024-12-11 19:13:49 -0800

    Changed some labels

--- src/yourdata.js
+++ src/yourdata.js
@@ -27,7 +27,7 @@ export default {
     //Line 1 For Header
     "fullstack developer",
     //Line 2 For Header
-    "visual designer",
+    "visual artist",
     //Line 3 For Header
     "cloud enthusiast",
   ],
@@ -44,7 +44,7 @@ export default {
     {
       id: 1, //DO NOT CHANGE THIS (Please)😅
       title: "PSL Group", //Project Title - Add Your Project Title Here
-      para: "Software development on the FirstWord stack", // Add Your Service Type Here
+      para: "Senior Application Developer under the FirstWord project", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
       imageSrc:
         "https://i.imgur.com/M6R8LMH.png&auto=format&fit=crop&w=400&q=60",
@@ -53,13 +53,13 @@ export default {
     },
     {
       id: 2, //DO NOT CHANGE THIS (Please)😅
-      title: "Walt Disney Company", //Project Title - Add Your Project Title Here
-      para: "Visual Developer & Artist", // Add Your Service Type Here
+      title: "Dentist Génesis Cázares", //Project Title - Add Your Project Title Here
+      para: "Developing and designing Dentist Dentist Génesis Cázares web page", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
       imageSrc:
-        "https://www.usatoday.com/gcdn/-mm-/091190c344f99fabe8e83297172ad0de8a040519/c=0-156-3072-1892/local/-/media/2017/09/22/USATODAY/USATODAY/636416893787170637-Legends-Plaza-Walt.JPG?width=660&height=373&fit=crop&format=pjpg&auto=webp&auto=format&fit=crop&w=400&q=60",
+        "https://i.imgur.com/369OAnr.jpeg&auto=format&fit=crop&w=400&q=90",
       //Project URL - Add Your Project Url Here
-      url: "https://tress.com.mx",
+      url: "https://dentistagenesiscazares.com/",
     },
     {
       id: 3, //DO NOT CHANGE THIS (Please)😅
@@ -89,34 +89,24 @@ export default {
       //Project URL - Add Your Project Url Here
       url: "https://umbrella-seguros.com",
     },
-    {
-      id: 6, //DO NOT CHANGE THIS (Please)😅
-      title: "Dentist Génesis Cázares", //Project Title - Add Your Project Title Here
-      para: "Developing and designing Dentist Dentist Génesis Cázares web page", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://i.imgur.com/369OAnr.jpeg&auto=format&fit=crop&w=400&q=90",
-      //Project URL - Add Your Project Url Here
-      url: "https://dentistagenesiscazares.com/",
-    },
   ],
   about: {
     paragraphs: [
       {
         id: 1,
-        text: `Greetings! I'm Cristopher Cervantes, a highly skilled and versatile professional with over 9 years of hands-on experience across a diverse range of development and design disciplines.`,
+        text: `Hi, I'm Cristopher Cervantes, a design and development professional with over nine years of experience across a variety of creative and technical disciplines. I've built a career out of translating innovative ideas into compelling digital solutions.`,
       },
       {
         id: 2,
-        text: `Throughout my career, I have excelled in developing applications that seamlessly operate both on premises and in the cloud. My proficiency extends to various programming languages, including C#, .NET, PHP, Python, NodeJS, and React.`,
+        text: `I've worked across on-premises and cloud environments, developing applications that solve complex technical challenges. My technical toolkit includes languages like C#, .NET, PHP, Python, NodeJS, and React, allowing me to craft versatile and robust software solutions.`,
       },
       {
         id: 3,
-        text: `Rather than defining myself solely by past accomplishments, I embrace a forward-looking perspective. I believe in continually expanding my knowledge, taking on new challenges, and pursuing meaningful endeavors.`,
+        text: `Instead of focusing only on what I’ve done in the past, I like to keep my eyes on the future. I’m all about learning new things, tackling challenges, and going after goals that truly matter.`,
       },
       {
         id: 4,
-        text: `I have an insatiable appetite for learning, which drives me to stay at the forefront of technology and industry trends. This passion for continuous improvement motivates me to explore innovative and impactful projects.`,
+        text: `I have an insatiable appetite to keep learning, this drives to me to stay at the forefront of technology and new industry trends. My passion for continuous improvement motivates to search for innovative and projects that have an impact on the world.`,
       },
     ],
     image: photo,
@@ -126,43 +116,43 @@ export default {
       id: 1,
       img: awsIcon,
       para:
-        "With my proficient AWS Cloud skills, I can revolutionize the way we handle projects by automating, testing, building, and delivering them rapidly and continuously. By harnessing the power of AWS, we ensure that your users consistently access the latest and most stable versions of your product.",
+        "With my knowledge of AWS services, we can build easy-to-use and scalable projects that will be ready for testing, builing and delivering them in a continuous and optimized way. By harnessing the power of the cloud we can ensure that your users have consistent access to your latest and most stable versions of your products.",
     },
     {
       id: 2,
       img: codeIcon,
       para:
-        "From Lambda to .NET, the backend serves as the foundational pillar for every application. Together, let's construct a robust framework for your software solutions, ensuring a powerful and reliable structure.",
+        "From Lambda all the way to .NET, backend if the foundation of every application. Together let's build a strong and robust framework for your products, ensuring performance and reliability. ",
     },
     {
       id: 3,
       img: reactIcon,
       para:
-        "With my proficiency in React, we can craft responsive, modern, and stylized user interfaces while ensuring an easy-to-maintain and highly efficient web project.",
+        "With my React skills, we can build a responsive, modern and stylized interfaces that not only feel and look amazing, but also are easy-to-maintain.",
     },
     {
       id: 4,
       img: designIcon,
       para:
-        "From designing captivating web pages to animating engaging promo videos for your product, I possess the expertise to breathe life into your ideas and turn them into reality!",
+        "From designing captivating web pages, all the way to animating your mascot in a promo video, I possess the expertise to bring your brand to life. Let's work together to create a unique and memorable experience for your users.",
     },
     {
       id: 5,
       img: jsIcon,
       para:
-        "If React isn't your preference and you seek to enhance your current website, my expertise in JavaScript and CSS can revitalize your brand without altering your existing designs. Let's work together to elevate your online presence and create a seamless user experience.",
+        "If React is not your cup of tea and you solely want to enhance your current website, my expertise with JavaScript and CSS can revitalize your brand, giving your product a fresh new coat of paint.",
     },
     {
       id: 6,
       img: analyticsIcon,
       para:
-        "Leveraging web analytics tools, we can gain valuable insights into how your users interact with your product or brand, enabling data-driven decisions to enhance performance and user experience.",
+        "An application is as reliable as the developer who built it. With my experience using web analytics and security tools I can ensure that your application would not only work and look great, it will also be secure, reliable and scalable.",
     },
   ],
 
   interestsHeading: "Interests",
   interestsPara:
-    "I am actively pursuing collaborative opportunities with companies, agencies, and individuals. My goal is not simply to work for them but to work with them, bringing my wealth of experience to the table. Together, we can synergistically address real business challenges, harnessing the collective power of our diverse expertise and knowledge.",
+    "I am looking to collaborate along companies agencies and inviduals. Not to simply work for them, but to work with them, bringing my experience to the table. Together we can tackle real business and technological challenges, harnessing our collective expertise and knowledge.",
   // End interests Section -----------------
 
   //   Contact Section --------------
